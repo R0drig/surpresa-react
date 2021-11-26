065fab58c495decf27af9ecb21b42b7d4be2f363
R0drig <rdrpo2014@gmail.com>
2021-11-26 04:09:08 -0300

    Added Mensage and MensagesPage files

---
+++ src/components/Mensage.jsx
@@ -0,0 +1,12 @@
+import {memo} from 'react'
+
+const Mensage = (props) => {
+  const {id,author,mensage} = props.data
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default memo(Mensage)
